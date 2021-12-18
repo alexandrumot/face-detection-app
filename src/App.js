cd960c1f28572a56afee1fdc125651199e5d8cac
Hackerutz <Hackerutz.21@gmail.com>
2021-12-18 20:34:31 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceDetection from './components/FaceRecognition/FaceRecognition';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  function onInputChange(event) {
+    setInput(event.target.value);
+  }
+
+  function onButtonSubmit() {
+    setImageUrl(input);
+    console.log('click!');
+  }
+
   return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/*<FaceDetection />*/}
+      <ImageLinkForm 
+        onInputChange={onInputChange} 
+        onButtonSubmit={onButtonSubmit}/>
+      <FaceDetection imageUrl={imageUrl}/>
     </div>
   );
 }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FaceRecognition = ({imageUrl}) => {
+    return (
+        <div className='center ma'>
+            <div className='absolute mt2'>
+
+            </div>
+            <img alt='image'src={imageUrl} width='500px' height='auto' />
+        </div>
+    );
+}
+
+export default FaceRecognition;
