58b1cea6c2d877c751cba723bbd82808baa71506
Hackerutz <Hackerutz.21@gmail.com>
2022-01-09 17:39:05 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -6,71 +6,87 @@ import Rank from './components/Rank/Rank';
 import FaceDetection from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
-import { useState } from 'react';
+import React from 'react';
 // import Clarifai from 'clarifai';
 
 // const app = new Clarifai.App({
-//   apiKey: '50aecfd11d0a4384b35fede60fca19cf'
-// });
+  //   apiKey: '50aecfd11d0a4384b35fede60fca19cf'
+  // });
 
-
-function App() {
-  const [input, setInput] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
-  const [route, setRoute] = useState('signin');
-  const [isSignedIn, setIsSignedIn] = useState(false);
-
-  function onInputChange(event) {
-    setInput(event.target.value);
-  }
-
-  function onButtonSubmit() {
-    setImageUrl(input);
-    // app.models.predict(Clarifai.GENERAL_MODEL, input).then(
-    //   function(response) {
-    //     console.log(response);
-    //   },
-    //   function(err) {
-    //     console.log(err);
-    //   }
-    // );
-    // app.models.predict(
-    //     Clarifai.FACE_DETECT_MODEL,
-    //     input)
-    //   .then(response => console.log(response))
-    //   .catch(err => console.log(err));
-  }
-
-  function onRouteChange(route) {
-    if (route === 'signout') {
-      setIsSignedIn(false);
-    } else if (route === 'home') {
-      setIsSignedIn(true);
-    }
-    setRoute(route);
-  }
-
-  return (
-    <div className="App">
-      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange}/>
-      { route === 'home' 
-        ? <div>
-            <Logo />
-            <Rank />
-            <ImageLinkForm 
-              onInputChange={onInputChange} 
-              onButtonSubmit={onButtonSubmit}
-            />
-            <FaceDetection imageUrl={imageUrl} />
-          </div>
-        : (
-          route === 'signin'
-          ? <Signin onRouteChange={onRouteChange}/>
-          : <Register onRouteChange={onRouteChange}/>
-        )      
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: '',
+      imageUrl: '',
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id:'',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
       }
-    </div>
-  );
+    }
+    this.onInputChange = this.onInputChange.bind(this)
+    this.onButtonSubmit = this.onButtonSubmit.bind(this)
+    this.onRouteChange = this.onRouteChange.bind(this)
+    this.loadUser = this.loadUser.bind(this)
+  }
+
+  loadUser = (data) => {
+    this.setState({ 
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value })
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({ route: route })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+        { this.state.route === 'home' 
+          ? <div>
+              <Logo />
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceDetection imageUrl={this.state.imageUrl} />
+            </div>
+          : (
+            this.state.route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
+            : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
+          )      
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
